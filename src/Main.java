a032ec492abe4346f5de69ad1ecdb61ef1aa6e24
Shaam26 <shameembanu561@gmail.com>
2026-06-14 19:14:05 +0530

    Updated file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,82 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+
+public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        // java banking program
+        double balance=0;
+        boolean isRunning = true;
+        int choice;
+
+        while (isRunning) {
+            System.out.println("***********");
+            System.out.println("Banking Program");
+            System.out.println("***********");
+            System.out.println("1.Show Balance");
+            System.out.println("2.deposit");
+            System.out.println("3.Withdraw");
+            System.out.println("4.exit");
+            System.out.println("***********");
+
+            System.out.println("Enter your choice(1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance +=  deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                case 5 -> System.out.println("INVALID CHOICE ");
+            }
+
+        }
+
+
+        System.out.println("***************************");
+        System.out.println("Thank You! have a nice day!");
+        System.out.println("***************************");
+
+
+        scanner.close();
+    }
+    static void showBalance(double balance){
+        System.out.println("***********");
+        System.out.printf("$%.2f\n",balance);
+
+    }
+    static double deposit() {
+        double amount;
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+
+        if (amount < 0) {
+            System.out.println("amount can't be negative");
+            return 0;
+        } else {
+            return amount;
         }
     }
-}
+
+        static double withdraw(double balance){
+            double amount;
+            System.out.println("Enter amount to be withdrawn: ");
+            amount = scanner.nextDouble();
+
+            if(amount > balance){
+                System.out.println("INSUFFICIENT FUNDS");
+                return 0;
+            }else if(amount < 0){
+                System.out.println("Amount can't be negative");
+                return 0;
+            }else{
+                return amount;
+            }
+        }
+
+    }
+
+
